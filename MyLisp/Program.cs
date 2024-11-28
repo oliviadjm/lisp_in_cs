293b9acf7e5c11395dc51e53547e5329a6a3eef8
Olivia Jones <odjones1@crimson.ua.edu>
2024-11-27 19:43:11 -0600

    sprint 1 complete!

---
+++ MyLisp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+class MyLisp {
+    static string read(string input){
+        return input;
+    }
+
+    static string eval(string input) {
+        return input;
+    }
+
+    static string print(string input) {
+        return input;
+    }
+
+    static string repl(string input) {
+        return print(eval(read(input)));
+    }
+
+    static void Main(string[] args) {
+        while(true) {
+            try {
+                Console.Write("user> ");
+                string input = Console.ReadLine();
+                Console.WriteLine(repl(input));
+            }
+            catch(IOException e) {
+                Console.WriteLine("IOException");
+                break;
+            }
+                
+        }
+        
+    }
+}
+
